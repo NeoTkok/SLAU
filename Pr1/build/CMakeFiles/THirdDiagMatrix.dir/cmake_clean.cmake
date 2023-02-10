8bcf6bdfff4d06a3d3f002c0ffce0888324d7747
NeoDima <murdereruu@yandex.ru>
2023-02-10 22:59:29 +0300

    3DiagMatrix+Cmake

---
+++ Pr1/build/CMakeFiles/THirdDiagMatrix.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/THirdDiagMatrix.dir/main.cpp.o"
+  "CMakeFiles/THirdDiagMatrix.dir/main.cpp.o.d"
+  "THirdDiagMatrix"
+  "THirdDiagMatrix.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/THirdDiagMatrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Pr1/build/CMakeFiles/THirdDiagMatrix.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for THirdDiagMatrix.
+# This may be replaced when dependencies are built.
